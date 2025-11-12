686bf08c9ed21a380797f8573def00ebbb3479c2
Nguyenthuyhangm <23020662@vnu.edu.vn>
2025-11-12 16:21:51 +0700

    Remove node_modules from Git and ignore it

---
+++ be/ai-tour-guide-backend/supabse.js
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+export const supabase= createClient(
+ "https://xiscqnfbxkfzdpbnsgcg.supabase.co",
+ "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhpc2NxbmZieGtmemRwYm5zZ2NnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4Mzk3NzUsImV4cCI6MjA3ODQxNTc3NX0.08a26-Z9P-44Nv-skWaTFBGQSwtkP_J5xFRG9Hbm6iE"   
+);
